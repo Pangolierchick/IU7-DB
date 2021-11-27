f2afb1dae9e3830c5c6d9b4f03ad9879c170efa5
Pangolierchick <dontgofun@gmail.com>
2021-11-27 03:34:03 +0300

    lab 06

---
+++ lab_06/src/db.py
@@ -0,0 +1,184 @@
+import psycopg2
+import uuid
+
+class SteamDBase:
+    def __init__(self, password):
+        self.connection = psycopg2.connect(database='db_labs', user='postgres', password=password)
+    
+    def __del__(self):
+        self.connection.close()
+    
+    def scalar(self):
+        with self.connection.cursor() as cur:
+            cur.execute(
+                '''
+                select avg(price) from inventory;
+                '''
+            )
+            data = cur.fetchall()
+
+        self.connection.commit()
+
+        return data
+
+    def join(self):
+        with self.connection.cursor() as cur:
+            cur.execute(
+                '''
+                select accs.id, sum(inventory.price)
+                from accs
+                join inventory on accs.id=inventory.user_id
+                group by accs.id;
+                '''
+            )
+
+            data = cur.fetchall()
+        
+        return data
+    
+    def cte(self):
+        with self.connection.cursor() as cur:
+            cur.execute(
+                '''
+                with cte (row_nu, id, appid, price) as (
+                select row_number() over (partition by inventory.appid order by inventory.appid), id, appid, price
+                from inventory)
+
+                select *
+                from cte
+                where row_nu = 1;
+                '''
+            )
+
+            data = cur.fetchall()
+        
+        return data
+
+    def meta(self):
+        with self.connection.cursor() as cur:
+            cur.execute(
+                '''
+                SELECT pg.oid, pg.datconnlimit FROM pg_database pg WHERE pg.datname = 'db_labs';
+                '''
+            )
+
+            data = cur.fetchall()
+        
+        return data
+
+    def scalar3(self):
+        with self.connection.cursor() as cur:
+            cur.execute(
+                '''
+                -- create or replace function get_price(gifted integer, price integer)
+                -- returns integer as $$
+                -- begin
+                --     return price * gifted;
+                -- end;
+                -- $$ language plpgsql;
+
+                select appid, get_price(gifted, price)
+                from inventory;
+                '''
+            )
+
+            data = cur.fetchall()
+        
+        return data
+
+    def table3(self):
+        with self.connection.cursor() as cur:
+            cur.execute(
+                '''
+                 create or replace function find_user_games(_id bigint)
+                 returns table(
+                 	id bigint,
+                 	appid bigint,
+                 	name varchar
+                 ) as $$
+
+                 begin
+                 	create temp table if not exists games (
+                 		id bigint,
+                 		appid bigint,
+                 		name varchar
+                 	);
+              
+                 	insert into games (id, appid, name)
+                 	select inventory.user_id, inventory.appid, apps.name
+                 	from inventory join apps on inventory.appid = apps.id 
+                 	where inventory.user_id = _id;
+                    
+                 	return query
+                 	select * from games;
+                 end;
+                 $$ language plpgsql;
+
+                select *
+                from find_user_games(76561198070966937);
+                '''
+            )
+
+            data = cur.fetchall()
+        
+        return data
+
+    def proc3(self):
+        with self.connection.cursor() as cur:
+            cur.execute(
+                '''
+                -- create or replace procedure change_playtime(uid uuid, w int, m int, l int)
+                -- as $$
+                -- begin
+                --     update playtime
+                --     set forever = forever + w + m + l,
+                --     windows = windows + w,
+                --     mac = mac + m,
+                --     linux = linux + l
+                --     where playtime.id = uid;
+                -- end;
+                -- $$ language plpgsql;
+
+                call change_playtime('0003809f-3d1f-4be8-adb1-8fbfefb446a4', 30, 0, 20)
+                '''
+            )
+
+    def func(self):
+        with self.connection.cursor() as cur:
+            cur.execute(
+                '''
+                select current_date;
+                '''
+            )
+
+            data = cur.fetchall()
+        
+        return data
+
+    def create_table(self):
+        with self.connection.cursor() as cur:
+            cur.execute(
+                '''
+                create table if not exists Prod(
+                    id uuid primary key,
+                    name varchar not null,
+                    price int CHECK (price >= 0) not null
+                );
+                '''
+            )
+
+        self.connection.commit()
+        
+    
+    def insert(self, n, p):
+        cur_uuid = uuid.uuid4()
+
+        with self.connection.cursor() as cur:
+            cur.execute(
+                f'''
+                insert into Prod(id, name, price) 
+                values ('{cur_uuid}', '{n}', '{p}');
+                '''
+            )
+
+        self.connection.commit()
---
+++ lab_06/src/main.py
@@ -0,0 +1,68 @@
+from db import SteamDBase
+import configparser
+
+class transaction:
+    def __init__(self, n, f):
+        self.name = n
+        self.func = f
+    
+    def __call__(self):
+        return self.func()
+
+def read_cfg(path:str):
+    config = configparser.ConfigParser()
+    config.read(path)
+
+    return config
+
+def get_password(cfg:dict):
+    return cfg['dbase']['password']
+
+def insert_wrapper(db:SteamDBase):
+    def clojure():
+        name = input("Введите название: ")
+        price = int(input("Введите стоимость: "))
+
+        return db.insert(name, price)
+
+    return clojure
+
+def main():
+    cfg = read_cfg('../config.ini')
+    dbase = SteamDBase(get_password(cfg))
+    funcs = [
+            transaction("Cкалярный запрос", dbase.scalar),
+            transaction('Запрос с несколькими соединениями (JOIN)', dbase.join),
+            transaction('Запрос с ОТВ(CTE) и оконными функциями', dbase.cte),
+            transaction('Запрос к метаданным', dbase.meta),
+            transaction('Скалярная функция (написанная в третьей лабораторной работе)', dbase.scalar3),
+            transaction('Многооператорная или табличная функция (написанная в третьей лабораторной работе)', dbase.table3),
+            transaction('Хранимая процедура (написанная в третьей лабораторной работе)', dbase.proc3),
+            transaction('Cистемная функция или процедура', dbase.func),
+            transaction('Создать таблицу в базе данных, соответствующую тематике БД', dbase.create_table),
+            transaction('Выполнить вставку данных в созданную таблицу с использованием инструкции INSERT', insert_wrapper(dbase))
+            ]
+
+    while True:
+        print("0  --- Выход из приложения;")
+
+        for i, f in enumerate(funcs):
+            print(f"{i + 1} --- {f.name};")
+
+        chosen = int(input("Выберите пункт меню: "))
+
+        if chosen == 0:
+            print("Exitting")
+            break
+        if 0 <= chosen - 1 < len(funcs):
+            print("Calling ... ")
+            res = funcs[chosen - 1]()
+            
+            if res is not None:
+                for i in res:
+                    print(*i)
+
+        
+
+if __name__ == '__main__':
+    main()
